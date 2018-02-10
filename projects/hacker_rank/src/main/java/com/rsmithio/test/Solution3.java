bdab3213a86fe169ef296235ed95e52489f46010
Robin Smith <robin@rsmith.io>
2018-02-10 16:09:23 +0000

    Add hacker rank solutions.

---
+++ projects/hacker_rank/src/main/java/com/rsmithio/test/Solution3.java
@@ -0,0 +1,37 @@
+package com.rsmithio.test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution3 {
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            ArrayList<Integer> series = calculate_series(a, b, n);
+            ArrayList<String> str_series = series.stream().map(j -> j.toString()).collect(Collectors.toCollection(ArrayList::new));
+            System.out.println(String.join(" ", str_series));
+        }
+        in.close();
+    }
+
+    public static ArrayList<Integer> calculate_series(int a, int b, int n) {
+        ArrayList<Integer> series = new ArrayList<Integer>();
+
+        int value;
+        for (int i = 0; i < n; ++i) {
+            value = b * (int) Math.pow(2, i);
+
+            if (i > 0) {
+                value += series.get(i - 1);
+            }
+            series.add(value);
+        }
+
+        return series.stream().map(j -> a + j).collect(Collectors.toCollection(ArrayList::new));
+    }
+}
---
+++ projects/hacker_rank/src/test/java/com/rsmithio/test/Solution3Test.java
@@ -0,0 +1,64 @@
+package com.rsmithio.test;
+
+
+import java.util.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Solution3Test {
+    @Test
+    public void testShouldCalculateSeriesWith_1_1_1() {
+        ArrayList<Integer> series = Solution3.calculate_series(1, 1, 1);
+
+        ArrayList<Integer> expected = new ArrayList<Integer>();
+        expected.add(2);
+
+        Assert.assertEquals(expected, series);
+    }
+
+    @Test
+    public void testShouldCalculateSeriesWith_1_1_2() {
+        ArrayList<Integer> series = Solution3.calculate_series(1, 1, 2);
+
+        ArrayList<Integer> expected = new ArrayList<Integer>();
+        expected.add(2);
+        expected.add(4);
+
+        Assert.assertEquals(expected, series);
+    }
+
+    @Test
+    public void testShouldCalculateSeriesWith_5_3_5() {
+        ArrayList<Integer> series = Solution3.calculate_series(5, 3, 5);
+
+        ArrayList<Integer> expected = new ArrayList<Integer>();
+        expected.add(8);
+        expected.add(14);
+        expected.add(26);
+        expected.add(50);
+        expected.add(98);
+
+        Assert.assertEquals(expected, series);
+    }
+
+    @Test
+    public void testShouldCalculateSeriesWith_0_2_10() {
+        ArrayList<Integer> series = Solution3.calculate_series(0, 2, 10);
+
+        ArrayList<Integer> expected = new ArrayList<Integer>();
+        expected.add(2);
+        expected.add(6);
+        expected.add(14);
+        expected.add(30);
+        expected.add(62);
+        expected.add(126);
+        expected.add(254);
+        expected.add(510);
+        expected.add(1022);
+        expected.add(2046);
+
+        Assert.assertEquals(expected, series);
+    }
+}
